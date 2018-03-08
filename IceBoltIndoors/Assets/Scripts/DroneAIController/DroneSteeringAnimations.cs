b69d39c2872ce6e57908cc5519d8451b4377ab0e
drwiner <drwiner131@gmail.com>
2018-03-07 18:27:47 -0700

    drone steering example

---
+++ IceBoltIndoors/Assets/Scripts/DroneAIController/DroneSteeringAnimations.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using SteeringNamespace;
+
+public class DroneSteeringAnimations : MonoBehaviour
+{
+    private Animator thisMecanim;
+    private DynoBehavior_TimelineControl DBTC;
+    //Vector3 currentForce;
+    //float currentTorque;
+
+    void Start()
+    {
+        thisMecanim = GetComponent<Animator>();
+        DBTC = GetComponent<DynoBehavior_TimelineControl>();
+    }
+
+    void Update()
+    {
+        //thisMecanim.SetFloat("angle") = DBTC.torque;
+        var droneDirection = transform.TransformDirection(DBTC.force);
+        thisMecanim.SetFloat("angle", droneDirection.z);
+        thisMecanim.SetFloat("forward", -droneDirection.x);
+
+    }
+
+    
+}
