b1af636cbbbec4f3cb5653499ea7a73555de2588
drwiner <drwiner131@gmail.com>
2018-03-13 18:52:13 -0600

    fire animation

--- IceBoltIndoors/Assets/Scripts/BasicScriptsForScene/laserScript.cs
+++ IceBoltIndoors/Assets/Scripts/BasicScriptsForScene/laserScript.cs
@@ -6,8 +6,11 @@ public class laserScript : MonoBehaviour {
 	public Transform endPoint;
     public Material laserMaterial;
 	LineRenderer laserLine;
+    public ParticleSystem PS;
+
+    public bool firing = false;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		laserLine = GetComponent<LineRenderer> ();
 		laserLine.SetWidth (.1f, .1f);
         laserLine.material = laserMaterial;
@@ -15,9 +18,46 @@ public class laserScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		laserLine.SetPosition (0, startPoint.position);
-		laserLine.SetPosition (1, endPoint.position);
+        if (firing)
+        {
+            Fire();
+        }
+        else
+        {
+            Stop();
+        }
 
 	}
+
+    public void Fire()
+    {
+        if (!PS.isPlaying)
+        {
+            laserLine.enabled = false;
+
+            var main = PS.main;
+            var distance = Vector3.Distance(startPoint.position, endPoint.position);
+            Debug.Log(distance);
+            var neededTime = distance / main.startSpeed.constantMax;
+
+            //PS.startLifetime = neededTime;
+
+            
+            //main.duration = neededTime;
+            //main.startSpeed = neededTime;
+            main.startLifetime = neededTime * 3;
+
+            PS.Play();
+            transform.LookAt(endPoint);
+            laserLine.SetPosition(0, startPoint.position);
+            laserLine.SetPosition(1, endPoint.position);
+        }
+    }
+
+    public void Stop()
+    {
+        PS.Stop();
+        laserLine.enabled = false;
+    }
 
 }
